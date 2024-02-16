c038b5d24371a8207f2565f7705e6ed3f7060865
RioFrancisAikman <115077816+RioFrancisAikman@users.noreply.github.com>
2024-02-16 14:37:54 +0000

    Created player movement
    Basic movement for moving in all directions with rotation, and able to jump

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed;
+    public float jumpForce = 8f;
+
+    public Rigidbody rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * moveSpeed;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        if (movement != Vector3.zero) //character rotation
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); //Specifying how I want the character to rotate
+            //animator.SetBool("IsMoving", true);      
+        }
+        else
+        {
+            //animator.SetBool("IsMoving", false);
+        }
+       
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //animator.SetBool("IsJumping", true);
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; 
+        }
+    }
+}
